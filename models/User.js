ad8815a0acc1ac40aff7022caca47d7f453daaad
FACTS  LIMITED <adweshshaddie@gmail.com>
2020-10-07 11:13:49 +0300

    modify user model - add token and token expiration fields

--- models/User.js
+++ models/User.js
@@ -4,6 +4,8 @@ const uniqueValidator = require('mongoose-unique-validator')
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    resetToken: { type: String },
+    resetTokenExpiration: { type: Date },
     password: { type: String, required: true },
 }, { timestamps: true })
 
